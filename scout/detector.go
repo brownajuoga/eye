a3c96baaed76877b3fe8baa314b95e4930b97bbd
aabrown <broaaron45@gmail.com>
2026-03-12 00:55:05 +0300

    gocv detector

---
+++ scout/detector.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+	"gocv.io/x/gocv"
+)
+
+type MotionDetector struct{
+	bgSubtractor gocv.BackgroundSubtractorMOG2
+	threshold int
+}
+
+func NewMotionDetector(threshold int) *MotionDetector{
+	return &MotionDetector{
+		bgSubtractor: gocv.NewBackgroundSubtractorMOG2()
+		threshold: threshold,
+	}
+}
+
+func (m * MotionDetector) Detect(frame gocv.Mat) (bool, gocv.Mat) {
+	mask := gocv.NewMat()
+	m.bgSubtractor.Apply(frame, &mask)
+	movement := gocv.CountnonZero(mask)
+	return movement > m.threhold, mask
+}
